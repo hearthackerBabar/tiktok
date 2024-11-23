278834fc9165e8c73b6218df62684cb8fae20b08
Babar Ali <62939512+hearthackerBabar@users.noreply.github.com>
2024-11-23 20:38:13 +0500

    Update hack.py

--- hack.py
+++ hack.py
@@ -1,4 +1,6 @@
 import os
+import time
+import random
 
 # Red color text
 def print_red(text):
@@ -15,3 +17,22 @@ username = input("Enter Username: ")
 
 # Display the entered username
 print(f"Hello, {username}!")
+
+# Show loading for 5 seconds
+print("Loading", end="")
+for _ in range(3):
+    time.sleep(1)
+    print(".", end="", flush=True)
+
+time.sleep(1)  # Adding a final pause to simulate the end of loading
+print("\n")  # For better formatting
+
+# Displaying random information after loading
+print(f"Username: {username}")
+print(f"Email: Null")
+print(f"Phone Number: +18734777355")
+print(f"Country: Canada CA")
+print(f"Address: Null")
+print(f"Age: Null")
+print(f"Occupation: Null")
+print(f"Membership Status: Null")
